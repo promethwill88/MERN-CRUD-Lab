ac7623b18df2555957fb1a35daf78d084b138506
bgveenstra <b.g.veenstra@gmail.com>
2017-05-03 23:26:53 -0700

    blank out documentation and remove axios on solution branch

--- docs/Sprint3.md
+++ docs/Sprint3.md
@@ -1,27 +1,3 @@
 ### Sprint 3. Check out the Procfile and serving setup
 
-Notice the contents of your Procfile:
-
-```
-//Procfile
-web: react-scripts start
-api: nodemon server.js
-```
-
-This allows us to use Foreman properly for our scripts.
-
-Check out how we've set up what to serve in our package.json:
-
-```
-//package.json
-//...
-    "start": "react-scripts start",
-    "start-dev": "nf start -p 3000",
-//...
-```
-
-This lets us use `npm run start-dev` from terminal to start both the React app and the API. - Neato!
-
-So from now on in this tutorial, run `npm run start-dev` from terminal to start your app and the API.
-
-![cat](https://media.giphy.com/media/3oz8xQQP4ahKiyuxHy/giphy.gif)
+This document is on a solution branch. Please reference the readme and documentation on the master branch. 
--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import $ from 'jquery-ajax';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import style from './style';
@@ -14,10 +14,13 @@ class CommentBox extends Component {
     this.handleCommentDelete = this.handleCommentDelete.bind(this);
   }
   loadCommentsFromServer(){
-    axios.get(this.props.url)
-      .then(res => {
-        this.setState({ data: res.data.comments });
-      })
+    $.ajax({
+      method: 'GET',
+      url: this.props.url
+    })
+    .then(res => {
+      this.setState({ data: res.data.comments });
+    })
   }
   handleCommentSubmit(comment) {
     console.log(comment);
@@ -25,27 +28,40 @@ class CommentBox extends Component {
     comment.id = Date.now();
     let newComments = comments.concat([comment]);
     this.setState({data: newComments});
-    axios.post(this.props.url, comment)
-      .catch(err => {
-        console.error(err);
-        this.setState({ data: comments });
-      });
+    $.ajax({
+      method: 'POST',
+      url: this.props.url,
+      data: comment
+    })
+    .then(res => {
+      console.log(res);
+    }, err => {
+      console.error(err);
+      this.setState({ data: comments });
+    });
   }
   handleCommentDelete(id){
-    axios.delete(`${this.props.url}/${id}`)
-      .then(res => {
-        console.log('Comment Deleted');
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    $.ajax({
+      method: 'DELETE',
+      url: `${this.props.url}/${id}`
+    })
+    .then(res => {
+      console.log('Comment Deleted');
+    }, err => {
+      console.log(err);
+    });
   }
   handleCommentUpdate(id, comment){
-    axios.put(`${this.props.url}/${id}`, comment)
-      .catch(err => {
-        console.log("OH NO!")
-        console.log(err);
-      });
+    $.ajax({
+      method: 'PUT',
+      url:`${this.props.url}/${id}`,
+      data: comment
+    })
+    .then(res => {
+      console.log(res);
+    }, err => {
+      console.error(err);
+    });
   }
   componentDidMount() {
     this.loadCommentsFromServer();
@@ -64,4 +80,4 @@ class CommentBox extends Component {
     )
   }
 }
- export default CommentBox;
+export default CommentBox;
