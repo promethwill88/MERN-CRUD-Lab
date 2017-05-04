0849d69cb5e0c6235499e33b9bf5f180f5f65154
promethwill88 <wfong.bk@gmail.com>
2017-05-04 14:50:44 -0700

    POST works

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,19 +1,57 @@
 import React, {Component} from 'react';
+import $ from 'jquery-ajax';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
+import DATA from '../data';  // this uses seed data to test RENDER
 import style from './style';
 
 class CommentBox extends Component {
 	constructor(props){
 		super(props);
 		this.state = {data:[]};
+		this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+		this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+	}
+	loadCommentsFromServer() {
+		$.ajax({
+			method: 'GET',
+			url: this.props.url
+		})
+		.then((res) => {
+			this.setState({ data: res });
+		}, (err) => {
+			console.log('error', err)
+		})
+	}
+	handleCommentSubmit(comment) {
+		console.log('handleCommentSubmit is activated', comment);
+		let comments = this.state.data;
+		comment.id = Date.now();
+		let newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+		$.ajax({
+			method: 'POST',
+			url: this.props.url,
+			data: comment
+		})
+		.then(res => {
+			console.log(res);
+			
+		}, err => {
+			console.error(err)
+			this.setState({ data: comments });
+		});
+	}
+	commentDidMount() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	}
 	render() {
 		return(
 			<div style={ style.commentBox }>
 		    	<h2>Comments:</h2>
 			  <CommentList data={ this.state.data }/>
-			  <CommentForm />
+			   <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
 			  </div>
 		)
 	}
--- src/CommentForm.js
+++ src/CommentForm.js
@@ -18,6 +18,20 @@ class CommentForm extends Component {
   handleSubmit(e) {
     e.preventDefault();
     console.log(`${this.state.author} said "${this.state.text}"`)
+    let author = this.state.author.trim();
+    let text = this.state.text.trim();
+    if (!text || !author){
+      return;
+    }
+    this.props.onCommentSubmit({ 
+      author: author, 
+      text: text 
+    });
+    
+    this.setState({ 
+      author: '',
+      text: ''
+    });
     //we will be tying this into the POST method in a bit
   }
   render() {
